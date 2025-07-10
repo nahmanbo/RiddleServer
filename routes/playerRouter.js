34a0c62a52f392b2b8d578fc481470e793b3767c
nahmanbo <nhmanbo2003@gmail.com>
2025-07-10 10:24:03 +0300

    a

--- routes/playerRouter.js
+++ routes/playerRouter.js
@@ -1,18 +1,27 @@
 import { Router } from "express";
 const router = Router();
 
+// GET /players
 router.get("/", (req, res) => {
   res.send("Get all players");
 });
 
+// GET /players/:id
+router.get("/:id", (req, res) => {
+  res.send(`Get player with id ${req.params.id}`);
+});
+
+// POST /players
 router.post("/", (req, res) => {
   res.send("Add a new player");
 });
 
+// PUT /players/:id
 router.put("/:id", (req, res) => {
   res.send(`Update player with id ${req.params.id}`);
 });
 
+// DELETE /players/:id
 router.delete("/:id", (req, res) => {
   res.send(`Delete player with id ${req.params.id}`);
 });
